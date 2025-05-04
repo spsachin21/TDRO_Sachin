404490a58bc3f08dea0b2204ae39ca55403c7071
spsachin21 <88092354+spsachin21@users.noreply.github.com>
2025-05-04 17:38:25 +0530

    Update Train.py

--- code/Train.py
+++ code/Train.py
@@ -3,21 +3,21 @@ import torch.nn as nn
 
 import math
 import scipy.optimize as sopt
-import torch.nn.functional as F
-from torch.autograd import grad
+import torch.nn.functional as F  #This line imports the torch.nn.functional module, which contains a variety of functions commonly used in neural networks, such as activation functions (F.relu, F.sigmoid), loss functions (F.mse_loss, F.cross_entropy), and convolutional/pooling operations.
+from torch.autograd import grad # for automatic differentitaion in training
 
 def train_ERM(dataloader, model, optimizer):    
     model.train()
     for user_tensor, item_tensor in dataloader:
         optimizer.zero_grad()
-        loss = model.loss(user_tensor.cuda(), item_tensor.cuda())
-        loss.backward()
-        optimizer.step()
+        loss = model.loss(user_tensor.cuda(), item_tensor.cuda())  #computes a ranking loss (e.g., BPR, hinge, contrastive) or binary cross-entropy between predicted and true interactions.
+        loss.backward()  #Computes gradients of the loss with respect to all model parameters using autograd- This enables the model to learn how to update weights.
+        optimizer.step()  #update the model weights
     return loss
 
 def train_TDRO(dataloader, model, optimizer, n_group, n_period, loss_list, w_list, mu, eta, lamda, beta_p):
 
-    model.train()
+    model.train() #Sets the model to training mode, enabling features like gradient tracking
 
     # period importance
     m = nn.Softmax(dim=1) 
@@ -26,14 +26,14 @@ def train_TDRO(dataloader, model, optimizer, n_group, n_period, loss_list, w_lis
 
     for user_tensor, item_tensor, group_tensor, period_tensor in dataloader:
         optimizer.zero_grad()
-
+    #model's prediction error (sample_loss) and any regularization penalty (reg_loss)
         sample_loss, reg_loss = model.loss(user_tensor.cuda(), item_tensor.cuda())
 
         # calculate each group-period loss and group-period gradient
         loss_ge = torch.zeros((n_group,n_period)).cuda()
         grad_ge = torch.zeros((n_group,n_period,model.encoder_layer2.weight.reshape(-1).size(0))).cuda() #(linear layer input * output)
         for name, param in model.named_parameters():
-            if name == 'encoder_layer2.weight':
+            if name == 'encoder_layer2.weight':  # ensures that the following code block only executes for the weights of the layer named 'encoder_layer2'.
                 for g_idx in range(n_group):
                     for e_idx in range(n_period):
                         indices = ((group_tensor.squeeze(1))==g_idx)&(period_tensor.squeeze(1)==e_idx)
@@ -45,40 +45,40 @@ def train_TDRO(dataloader, model, optimizer, n_group, n_period, loss_list, w_lis
                         grad_ge[g_idx,e_idx] = grad_single
                         
         # worst-case factor
-        de = torch.tensor([torch.sum(group_tensor==g_idx) for g_idx in range(n_group)]).cuda()
-        loss_ = torch.sum(loss_ge,dim=1)
+        de = torch.tensor([torch.sum(group_tensor==g_idx) for g_idx in range(n_group)]).cuda()  # no. of datapoints across groups
+        loss_ = torch.sum(loss_ge,dim=1) # loss of a group across all periods dim=1 being const.< ==
         loss_ = loss_/(de+1e-16)
         
         # shifting factor
         trend_ = torch.zeros(n_group).cuda()
         for g_idx in range(n_group):
-            g_j = torch.mean(grad_ge[g_idx],dim=0) # sum up the period gradient for group 
-            sum_gie = torch.mean(grad_ge * beta_e, dim=[0,1])
-            trend_[g_idx] = g_j@sum_gie
+            g_j = torch.mean(grad_ge[g_idx],dim=0)  # sum up the period gradient for group ==> dim 0 const
+            sum_gie = torch.mean(grad_ge * beta_e, dim=[0,1]) 
+            trend_[g_idx] = g_j@sum_gie  
 
-        loss_ = loss_ * (1-lamda) + trend_ * lamda
+        loss_ = loss_ * (1-lamda) + trend_ * lamda 
 
         # loss consistency enhancement
         loss_[loss_==0] = loss_list[loss_==0]
         loss_list = (1 - mu) * loss_list + mu * loss_ 
 
         # group importance smoothing
-        update_factor = eta * loss_list
-        w_list = w_list * torch.exp(update_factor)
-        w_list = w_list/torch.sum(w_list)
+        update_factor = eta * loss_list # weight steo size
+        w_list = w_list * torch.exp(update_factor)   # it is wi value 
+        w_list = w_list/torch.sum(w_list)          
         loss_weightsum = torch.sum(w_list * loss_list)
 
         # add regularization loss
-        loss_weightsum = loss_weightsum + reg_loss
+        loss_weightsum = loss_weightsum + reg_loss 
 
         # back propagation and update parameters
-        loss_weightsum.backward()
-        optimizer.step()
+        loss_weightsum.backward()  #It calculates the gradients of the loss_weightsum with respect to all the model's trainable parameters
+        optimizer.step()    #updates the model's parameters based on the calculated gradients.
 
-        loss_list.detach_()
-        w_list.detach_()
+        loss_list.detach_()    #means it won't be considered for gradient calculations in future iterations. It's done to prevent unnecessary memory usage and potential errors.
+        w_list.detach_()  # same
 
-    with torch.no_grad():
+    with torch.no_grad():   #ensures that no gradient calculations are performed within it
         model.result[model.emb_id] = model.id_embedding[model.emb_id].data
         model.result[model.feat_id + model.num_user] = model.feature_extractor()[model.feat_id].data
 
